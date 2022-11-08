a35596004985f1aeeabcc9b95f2789e4501b12ec
Pablo Mariasch <103390999+Arteok@users.noreply.github.com>
2022-11-08 13:32:23 -0300

    08-11-2022

--- js/app.js
+++ js/app.js
@@ -1,37 +1,101 @@
 Chart.defaults.color = '#fff'
+Chart.defaults.borderColor = '#444'
+
+
 
 const printCharts = () => {
-    /*fetchCoastersData('https://coasters-api.herokuapp.com','https://coasters-api.herokuapp.com/country/Spain')
-    .then(([allCoasters, nationalCoasters]) => {
-            console.log(allCoasters, nationalCoasters)
-    })   */
 
-    renderModelsChart()
+    fetchCoastersData('https://coasters-api.herokuapp.com', 'https://coasters-api.herokuapp.com/country/Spain')
+        .then(([allCoasters, nationalCoasters]) => {
+            renderModelsChart(allCoasters);
+            renderFeaturesChart(nationalCoasters);
+            renderYearsChart(allCoasters);
+            enableEventHandlers(nationalCoasters);
+        })
 
 }
-const renderModelsChart = () => {
+
+
+
+const renderModelsChart = coasters => {
+
+    const uniqueModels = [...new Set(coasters.map(coaster => coaster.model))]
 
     const data = {
-        labels: ['uno', 'dos', 'tres'],
+        labels: uniqueModels,
         datasets: [{
-            data: [10,20,30],
+            data: uniqueModels.map(currentModel => coasters.filter(coaster => coaster.model === currentModel).length),
             borderColor: getDataColors(),
-            backgroundColor: getDataColors(40)
-            
-            //borderColor: ['red', 'green', 'blue'],
-            //backgroundColor: ['blue', 'green', 'red']
-        }]    
+            backgroundColor: getDataColors(20)
+        }]
     }
 
     const options = {
         plugins: {
-            legend: { position: 'left'}//pone las barras a la izquierda
+            legend: { position: 'left' }
         }
     }
 
-    new Chart('modelsChart',{type: 'line', data, options})//crea el tipo de grafico
+    new Chart('modelsChart', { type: 'doughnut', data, options })
 }
 
 
 
-printCharts()
+
+const renderFeaturesChart = coasters => {
+
+    const data = {
+        labels: coasters.map(coaster => coaster.name),
+        datasets: [{
+            label: 'Altura (m)',
+            data: coasters.map(coaster => coaster.height),
+            borderColor: getDataColors()[1],
+            backgroundColor: getDataColors(20)[1]
+        }]
+    }
+
+    const options = {
+        plugins: {
+            legend: { display: false }
+        },
+        scales: {
+            r: {
+                ticks: { display: false }
+            }
+        }
+    }
+
+    new Chart('featuresChart', { type: 'radar', data, options })
+}
+
+
+
+
+const renderYearsChart = coasters => {
+
+    const years = ['1998-2000', '2001-2003', '2004-2006', '2007-2009', '2013-2015', '2016-2018', '2019-2021']
+
+    const data = {
+        labels: years,
+        datasets: [{
+            data: getCoastersByYear(coasters, years),
+            tension: .5,//es para suavizar el grafico
+            borderColor: getDataColors()[1],
+            backgroundColor: getDataColors(20)[1],
+            fill: true,//para que tenga color de fondo
+            pointBorderWidth: 5
+        }]
+    }
+
+    const options = {
+        plugins: {
+            legend: { display: false }
+        }
+    }
+
+    new Chart('yearsChart', { type: 'line', data, options })
+}
+
+
+
+printCharts();
